efc3da8c4a62cbec5a88495b50bc6e0ed4e98187
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2023-07-22 13:55:12 -0300

    add oh-my-zsh script as unattended

--- zsh/install.sh
+++ zsh/install.sh
@@ -14,9 +14,21 @@ apt_install_packages
 
 echo "👩‍💻 Installing Oh My Zsh..."
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+echo "🎭 Installing spaceship Zsh theme..."
+
+git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+
+echo "🔤 Installing zsh-autosuggestions..."
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 echo "🔧 Configuring Zsh..."
+
+mv ${HOME}/.zshrc ${HOME}/.zshrc.bkp
+mv ${HOME}/.zsh_history ${HOME}/.zsh_history.bkp
 
 files_to_link=(".zshrc", ".zsh_history")
 
